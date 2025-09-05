b6b88afdbc6d4ebd1ce3875b6a8e47d89af1fde0
Patryk Stolinski <patrykstolinski@gmail.com>
2025-09-05 12:57:24 +0200

    refactoring: Added Skills ORM, adjusted Topics ORM.

--- models.py
+++ models.py
@@ -14,11 +14,34 @@ class Topic(db.Model):
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.Text)
     parent_topic_id = db.Column(UUID(as_uuid=False), db.ForeignKey("topics.id"), nullable=True)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
  
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
             "description": self.description,
-            "parentTopicId": self.parent_topic_id
+            "parentTopicID": self.parent_topic_id,
+            "createdAt": self.created_at
+        }
+
+class Skill(db.Model):
+    __tablename__ = "skills"
+    id = db.Column(UUID(as_uuid=False), primary_key=True, default=gen_uuid)
+    name = db.Column(db.String, nullable=False)
+    topic_id = db.Column(
+        UUID(as_uuid=False), 
+        db.ForeignKey("topics.id", ondelete="CASCADE"), 
+        nullable=False
+        )
+    difficulty = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "topicID": self.topic_id,
+            "difficulty": self.difficulty,
+            "createdAt": self.created_at
         }
